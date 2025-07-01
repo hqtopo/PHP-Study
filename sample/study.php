e646611bb454a8b378ea4e1dad2285e70534cc8a
302A-01\cre <a>
2025-07-01 12:19:55 +0900

    10だけ終わってない０000000

--- sample/study.php
+++ sample/study.php
@@ -1,4 +1,30 @@
 <?php
+echo "Hello World！\n";
+$var1 = "これは変数です";
+echo $var1;
+echo "\n";
+echo $var1;
+$var2 = "同じ変数を何度でも使用できます\n";
+echo $var2;
+echo $var2;
+echo $var2;
+$var2 = "変数は上書きが可能です\n";
+echo $var2;
+$str1 = '文字列1';
+$str1 .= '文字列2';
+echo $str1;
+echo "\n";
+$num1 = 5;
+$num2 = 15;
+
+if ($num1 < 10 && $num2 > 10) {
+    echo "num1は10未満かつnum2は10より大きい\n";
+}
+if ($num1 < 10 || $num2 < 10) {
+    echo "num1は10未満またはnum2は10未満\n";
+}
+
+// あいうえお
 // このファイルはphp study.phpで実行できます
 // これはコメントアウトです
 // コメントアウトはプログラムとして認識されず自由に書くことができます。
